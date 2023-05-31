b08bc042d68b3e2e2bf045199a8b0f68333a5cdb
Arpit <arpitkumar01923@gmail.com>
2023-05-31 20:44:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import img from '../public/loginImage.svg'
+
+function App() {
+  return (
+    <div className='mx-auto container'>
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
